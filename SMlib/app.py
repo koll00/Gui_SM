f68a60e467f51fba3f32f9199ab2cb0e5b518049
koll00 <senjia1988@126.com>
2013-12-18 22:20:08 +0800

    update

--- SMlib/app.py
+++ SMlib/app.py
@@ -2,13 +2,12 @@
 '''
 
 import sys
-sys.path.append('../')
-import SMGui as app
+sys.path.append('..\\')
 
+from SMlib import SMGui as app
 
 def main():
     app.run()
-    
     
 if __name__ == '__main__':
     main()
