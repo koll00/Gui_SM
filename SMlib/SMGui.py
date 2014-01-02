20fc7eec39440c8b17e27f1648a232e46f49c082
koll00 <senjia1988@126.com>
2014-01-02 11:06:29 +0800

    add function to add actions

--- SMlib/SMGui.py
+++ SMlib/SMGui.py
@@ -1,18 +1,33 @@
-import sys,os 
-sys.path.append('..'+ os.path.sep)
+import sys, os 
+sys.path.append('..' + os.path.sep)
 
-from PyQt4.QtGui import QMainWindow, QApplication
+from PyQt4.QtGui import QMainWindow, QApplication, QAction
 from PyQt4 import QtGui, QtCore
+from PyQt4.Qt import QKeySequence
 from PyQt4.QtCore import SIGNAL, Qt, QSize, QPoint
 import qrc_app
 
-from SMlib.configs.baseconfig import debug_print
+from SMlib.configs.baseconfig import debug_print, _
+from SMlib.configs.userconfig import NoDefault 
+from SMlib.configs.guiconfig import get_shortcut
 from SMlib.config import CONF
 from SMlib.py3compat import qbytearray_to_str
 from SMlib.configs.baseconfig import debug_print
+from SMlib.utils.qthelpers import (create_action, add_actions, get_icon,
+                                       get_std_icon, add_shortcut_to_tooltip,
+                                       create_module_bookmark_actions,
+                                       create_bookmark_action,
+                                       create_program_action, DialogManager,
+                                       keybinding, qapplication,
+                                       create_python_script_action, file_uri)
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(QMainWindow, self).__init__(parent)
+        
+        
+        # Shortcut management data
+        self.shortcut_data = []
         
         self.console = None
         self.workingdirectory = None
@@ -99,7 +114,7 @@ class MainWindow(QMainWindow):
         self.current_quick_layout = None
         self.previous_layout_settings = None
         self.last_plugin = None
-        self.fullscreen_flag = None # isFullscreen does not work as expected
+        self.fullscreen_flag = None  # isFullscreen does not work as expected
         # The following flag remember the maximized state even when 
         # the window is in fullscreen mode:
         self.maximized_flag = None
@@ -115,7 +130,7 @@ class MainWindow(QMainWindow):
         self.createMenus()
         self.createToolBars()
         self.createStatusBar()
-        #self.initalStatus()
+        # self.initalStatus()
         
         # Window set-up
         self.debug_print("Setting up window...")
@@ -125,7 +140,7 @@ class MainWindow(QMainWindow):
 #         self.newAct = QtGui.QAction(QtGui.QIcon(':/icons/new.png'), "&New",
 #                 self, shortcut=QtGui.QKeySequence.New,
 #                 statusTip="Create a new file", triggered=self.newFile)
-        #self.debug_print("  ..core action")
+        # self.debug_print("  ..core action")
         self.open_action = QtGui.QAction(QtGui.QIcon(':/icons/open.png'),
                 "&Open...", self, shortcut=QtGui.QKeySequence.Open,
                 statusTip="Open an existing file", triggered=self.open)
@@ -152,70 +167,79 @@ class MainWindow(QMainWindow):
         self.undo_action = QtGui.QAction(QtGui.QIcon(':/icons/undo.png'),
                                          "&Undo", self, shortcut="Ctrl+Z",
                                          statusTip="undo the operation",
-                                         triggered = self.undo)
+                                         triggered=self.undo)
         self.redo_action = QtGui.QAction(QtGui.QIcon(':/icons/redo.png'),
                                          "&Redo", self, shortcut="Ctrl+Y",
                                          statusTip="redo the operation",
-                                         triggered = self.redo)
-        self.edit_menu_actions = []
+                                         triggered=self.redo)
+        
+        self.edit_menu_actions = [self.undo_action, self.redo_action,
+                                      None, self.cut_action, self.copy_action,
+                                      self.paste_action]
+        
+        quit_action = QtGui.QAction(QtGui.QIcon(':/icons/redo.png'),
+                                         "&Quit", self, shortcut="Ctrl+Q",
+                                         statusTip="Quit",
+                                         triggered=self.redo)
+        self.file_menu_actions += [quit_action]
 #         self.newConsoleAct = QtGui.QAction(QtGui.QIcon(':/icons/new.png'), "&Console",
 #                 self, statusTip="Create a new console", triggered=self.newConsole)
 #         
 #         self.newMonitorAct = QtGui.QAction(QtGui.QIcon(':/icons/new.png'), "&Monitor",
 #                 self, statusTip="Create a new console", triggered=self.newMonitor)
-       
+        self.maximize_action = create_action(self, 'maximize',
+                                            triggered=self.maximize_dockwidget)
+        self.register_shortcut(self.maximize_action, "_",
+                                   "Maximize dockwidget", "Ctrl+Alt+Shift+M")
+        self.__update_maximize_action()
+            
+        # Fullscreen mode
+        self.fullscreen_action = create_action(self,
+                                            "Fullscreen mode",
+                                            triggered=self.toggle_fullscreen)
+        self.register_shortcut(self.fullscreen_action, "_",
+                                   "Fullscreen mode", "F11")
+        add_shortcut_to_tooltip(self.fullscreen_action, context="_",
+                                    name="Fullscreen mode")
+            
+        # Main toolbar
+        self.main_toolbar_actions = [self.maximize_action,
+                                         self.fullscreen_action
+                                         ]
+            
     def createMenus(self):
         'initial the menus for system'
-        #File menu
+        # File menu
         self.file_menu = self.menuBar().addMenu("&File")
-        self.new = self.file_menu.addMenu("&New")
-        #self.new.addAction(self.newConsoleAct)
-        #self.new.addAction(self.newMonitorAct)
-        
-#         self.file_menu.addAction(self.newAct)
-        self.file_menu.addSeparator();
-        self.file_menu.addAction(self.open_action)
-        self.file_menu.addAction(self.save_action)
-        
-        
-#        self.file_menu.addAction(self.saveAsAct)
-        
-#         self.file_menu.addAction(self.exitAct)
-        #Edit menu
+        # Edit menu
         self.edit_menu = self.menuBar().addMenu("&Edit")
-        self.edit_menu.addAction(self.undo_action)
-        self.edit_menu.addAction(self.redo_action)
-        self.edit_menu.addSeparator();
-        self.edit_menu.addAction(self.cut_action)
-        self.edit_menu.addAction(self.copy_action)
-        self.edit_menu.addAction(self.paste_action)
-        
-        self.view_menu = self.menuBar().addMenu("&View")
         # View menu
-        self.windows_toolbars_menu = QtGui.QMenu("Windows and toolbars", self)
-        self.connect(self.windows_toolbars_menu, SIGNAL("aboutToShow()"),self.update_windows_toolbars_menu)
-        self.view_menu.addMenu(self.windows_toolbars_menu)
-        
-        #Help menu
+        self.view_menu = self.menuBar().addMenu("&View")
+        # Help menu
         self.help_menu = self.menuBar().addMenu("&Help")
-#         self.helpMenu.addAction(self.aboutAct)
-#         self.helpMenu.addAction(self.aboutQtAct)
-            
+        
+        add_actions(self.file_menu, self.file_menu_actions)
+        add_actions(self.edit_menu, self.edit_menu_actions)
+        
+    
+    #---- Window setup
+    def create_toolbar(self, title, object_name, iconsize=24):
+        """Create and return toolbar with *title* and *object_name*"""
+        toolbar = self.addToolBar(title)
+        toolbar.setObjectName(object_name)
+        toolbar.setIconSize(QSize(iconsize, iconsize))
+        return toolbar
+    
     def createToolBars(self):
         'initial the tool bar'
-        self.fileToolBar = self.addToolBar("File")
-#        self.fileToolBar.addAction(self.new)
-        self.fileToolBar.addAction(self.open_action)
+        self.main_toolbar = self.create_toolbar("Main_toolbar", "main_toolbar")
+        self.file_toolbar = self.create_toolbar("File", "file")
+        self.edit_toolbar = self.create_toolbar("Edit", "edit")
         
-        self.editToolBar = self.addToolBar("Edit")
-        self.editToolBar.addAction(self.cut_action)
-        self.editToolBar.addAction(self.copy_action)
-        self.editToolBar.addAction(self.paste_action)
         
-        self.workSpace = QtGui.QToolBar('workSpace')
-        #self.workSpace.addAction(self.newConsoleAct)
-        #self.workSpace.addAction(self.newMonitorAct)
-        self.addToolBar(1, self.workSpace)
+        add_actions(self.main_toolbar, self.main_toolbar_actions)
+        add_actions(self.file_toolbar, self.file_toolbar_actions)
+        add_actions(self.edit_toolbar, self.edit_toolbar_actions)
         
     def createStatusBar(self):
         ''
@@ -268,6 +292,109 @@ class MainWindow(QMainWindow):
     def redo(self):
         print "redo"
         
+    def maximize_dockwidget(self, restore=False):
+        """Shortcut: Ctrl+Alt+Shift+M
+        First call: maximize current dockwidget
+        Second call (or restore=True): restore original window layout"""
+        if self.state_before_maximizing is None:
+            if restore:
+                return
+            # No plugin is currently maximized: maximizing focus plugin
+            self.state_before_maximizing = self.saveState()
+            focus_widget = QApplication.focusWidget()
+            for plugin in self.widgetlist:
+                plugin.dockwidget.hide()
+                if plugin.isAncestorOf(focus_widget):
+                    self.last_plugin = plugin
+            self.last_plugin.dockwidget.toggleViewAction().setDisabled(True)
+            self.setCentralWidget(self.last_plugin)
+            self.last_plugin.ismaximized = True
+            # Workaround to solve an issue with editor's outline explorer:
+            # (otherwise the whole plugin is hidden and so is the outline explorer
+            #  and the latter won't be refreshed if not visible)
+            self.last_plugin.show()
+            self.last_plugin.visibility_changed(True)
+            if self.last_plugin is self.editor:
+                # Automatically show the outline if the editor was maximized:
+                self.addDockWidget(Qt.RightDockWidgetArea,
+                                   self.outlineexplorer.dockwidget)
+                self.outlineexplorer.dockwidget.show()
+        else:
+            # Restore original layout (before maximizing current dockwidget)
+            self.last_plugin.dockwidget.setWidget(self.last_plugin)
+            self.last_plugin.dockwidget.toggleViewAction().setEnabled(True)
+            self.setCentralWidget(None)
+            self.last_plugin.ismaximized = False
+            self.restoreState(self.state_before_maximizing)
+            self.state_before_maximizing = None
+            self.last_plugin.get_focus_widget().setFocus()
+        self.__update_maximize_action()
+        
+    def __update_fullscreen_action(self):
+        if self.isFullScreen():
+            icon = "window_nofullscreen.png"
+        else:
+            icon = "window_fullscreen.png"
+        self.fullscreen_action.setIcon(get_icon(icon))
+        
+    def toggle_fullscreen(self):
+        if self.isFullScreen():
+            self.fullscreen_flag = False
+            self.showNormal()
+            if self.maximized_flag:
+                self.showMaximized()
+        else:
+            self.maximized_flag = self.isMaximized()
+            self.fullscreen_flag = True
+            self.showFullScreen()
+        self.__update_fullscreen_action()
+    
+    def register_shortcut(self, qaction_or_qshortcut, context, name,
+                          default=NoDefault):
+        """
+        Register QAction or QShortcut to Spyder main application,
+        with shortcut (context, name, default)
+        """
+        self.shortcut_data.append((qaction_or_qshortcut,
+                                    context, name, default))
+        self.apply_shortcuts()
+    
+    def remove_deprecated_shortcuts(self):
+        """Remove deprecated shortcuts"""
+        data = [(context, name) for (qobject, context, name,
+                default) in self.shortcut_data]
+        remove_deprecated_shortcuts(data)
+        
+    def apply_shortcuts(self):
+        """Apply shortcuts settings to all widgets/plugins"""
+        toberemoved = []
+        for index, (qobject, context, name,
+                    default) in enumerate(self.shortcut_data):
+            keyseq = QKeySequence(get_shortcut(context, name, default))
+            try:
+                if isinstance(qobject, QAction):
+                    qobject.setShortcut(keyseq)
+                elif isinstance(qobject, QShortcut):
+                    qobject.setKey(keyseq)
+            except RuntimeError:
+                # Object has been deleted
+                toberemoved.append(index)
+        for index in sorted(toberemoved, reverse=True):
+            self.shortcut_data.pop(index)
+    
+    def __update_maximize_action(self):
+        if self.state_before_maximizing is None:
+            text = "Maximize current plugin"
+            tip = "Maximize current plugin"
+            icon = "maximize.png"
+        else:
+            text = "Restore current plugin"
+            tip = "Restore plugin to its original size"
+            icon = "unmaximize.png"
+        self.maximize_action.setText(text)
+        self.maximize_action.setIcon(get_icon(icon))
+        self.maximize_action.setToolTip(tip)
+         
     def get_window_settings(self):
         """Return current window settings
         Symetric to the 'set_window_settings' setter"""
@@ -285,12 +412,12 @@ class MainWindow(QMainWindow):
     
     def setup_layout(self, default=False):
         """Setup window layout"""
-        #prefix = ('lightwindow' if self.light else 'window') + '/'
+        # prefix = ('lightwindow' if self.light else 'window') + '/'
         prefix = 'window/'
         (hexstate, window_size, prefs_dialog_size, pos, is_maximized,
          is_fullscreen) = self.load_window_settings(prefix, default)
         
-        #if hexstate is None and not self.light:
+        # if hexstate is None and not self.light:
         if hexstate is None:
             # First Spyder execution:
             # trying to set-up the dockwidget/toolbar positions to the best 
@@ -341,18 +468,18 @@ class MainWindow(QMainWindow):
         """Set window settings
         Symetric to the 'get_window_settings' accessor"""
         self.setUpdatesEnabled(False)
-        self.window_size = QSize(window_size[0], window_size[1]) # width,height
+        self.window_size = QSize(window_size[0], window_size[1])  # width,height
         self.prefs_dialog_size = QSize(prefs_dialog_size[0],
-                                       prefs_dialog_size[1]) # width,height
-        self.window_position = QPoint(pos[0], pos[1]) # x,y
+                                       prefs_dialog_size[1])  # width,height
+        self.window_position = QPoint(pos[0], pos[1])  # x,y
         self.setWindowState(Qt.WindowNoState)
         self.resize(self.window_size)
         self.move(self.window_position)
-        #if not self.light:
+        # if not self.light:
         if True:
             # Window layout
             if hexstate:
-                self.restoreState( QByteArray().fromHex(str(hexstate)) )
+                self.restoreState(QByteArray().fromHex(str(hexstate)))
                 # [Workaround for Issue 880]
                 # QDockWidget objects are not painted if restored as floating 
                 # windows, so we must dock them before showing the mainwindow.
@@ -376,15 +503,15 @@ class MainWindow(QMainWindow):
         with *prefix*, under *section*
         default: if True, do not restore inner layout"""
         get_func = CONF.get_default if default else CONF.get
-        window_size = get_func(section, prefix+'size')
-        prefs_dialog_size = get_func(section, prefix+'prefs_dialog_size')
+        window_size = get_func(section, prefix + 'size')
+        prefs_dialog_size = get_func(section, prefix + 'prefs_dialog_size')
         if default:
             hexstate = None
         else:
-            hexstate = get_func(section, prefix+'state', None)
-        pos = get_func(section, prefix+'position')
-        is_maximized =  get_func(section, prefix+'is_maximized')
-        is_fullscreen = get_func(section, prefix+'is_fullscreen')
+            hexstate = get_func(section, prefix + 'state', None)
+        pos = get_func(section, prefix + 'position')
+        is_maximized = get_func(section, prefix + 'is_maximized')
+        is_fullscreen = get_func(section, prefix + 'is_fullscreen')
         return hexstate, window_size, prefs_dialog_size, pos, is_maximized, \
                is_fullscreen
                
@@ -394,18 +521,27 @@ class MainWindow(QMainWindow):
         win_size = self.window_size
         prefs_size = self.prefs_dialog_size
         
-        CONF.set(section, prefix+'size', (win_size.width(), win_size.height()))
+        CONF.set(section, prefix + 'size', (win_size.width(), win_size.height()))
 #        CONF.set(section, prefix+'prefs_dialog_size',(prefs_size.width(), prefs_size.height()))
-        CONF.set(section, prefix+'is_maximized', self.isMaximized())
-        CONF.set(section, prefix+'is_fullscreen', self.isFullScreen())
+        CONF.set(section, prefix + 'is_maximized', self.isMaximized())
+        CONF.set(section, prefix + 'is_fullscreen', self.isFullScreen())
         pos = self.window_position
-        CONF.set(section, prefix+'position', (pos.x(), pos.y()))
+        CONF.set(section, prefix + 'position', (pos.x(), pos.y()))
 #         if not self.light:
 #             self.maximize_dockwidget(restore=True)# Restore non-maximized layout
 #             qba = self.saveState()
 #             CONF.set(section, prefix+'state', qbytearray_to_str(qba))
 #             CONF.set(section, prefix+'statusbar',not self.statusBar().isHidden())
-            
+    
+    def add_dockwidget(self, child):
+        """Add QDockWidget and toggleViewAction"""
+        dockwidget, location = child.create_dockwidget()
+        if CONF.get('main', 'vertical_dockwidget_titlebars'):
+            dockwidget.setFeatures(dockwidget.features() | 
+                                   QDockWidget.DockWidgetVerticalTitleBar)
+        self.addDockWidget(location, dockwidget)
+        self.widgetlist.append(child)
+        
     def resizeEvent(self, event):
         """Reimplement Qt method"""
         if not self.isMaximized() and not self.fullscreen_flag:
@@ -423,7 +559,7 @@ class MainWindow(QMainWindow):
             icon = "window_nofullscreen.png"
         else:
             icon = "window_fullscreen.png"
-        #self.fullscreen_action.setIcon(get_icon(icon))
+        # self.fullscreen_action.setIcon(get_icon(icon))
         
 def run():
     ''
